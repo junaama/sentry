081346d5fb82d68ed1e90abc4e8afb5d2d602273
junaama <0naama0@gmail.com>
2021-03-27 22:01:22 -0400

    feat(add firebase database)

---
+++ sentryfrontend/src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
+  };
+firebase.initializeApp(config);
+export default firebase;
